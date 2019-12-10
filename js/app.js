325d2392ce4f480a588f321118e73171f8f2f59c
Hung Ly <hunghly@gmail.com>
2019-12-09 20:43:32 -0600

    reworked display when button clicked

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@
     // If login button is clicked & account login is valid, hide the login page and proceed to the main menu
     let loginButtonClicked = function() {
         loginButtonEl.addEventListener("click", function() {
-           loginPageEl.hidden = true;
-           homePageEl.hidden = false;
+            loginPageEl.style.display = "none";
+            homePageEl.style.display = "block";
         });
     };
 
