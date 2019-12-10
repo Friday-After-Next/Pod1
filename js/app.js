a17d53203a929a4e13aabafc0821c1b408b3bd7e
Hung Ly <hunghly@gmail.com>
2019-12-09 18:02:37 -0600

    started first page/set up some transitioning

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 (function() {
-    "use strict"
+    "use strict";
     // Enter code here inside of IIFE
+    let loginPageEl = document.getElementById("login-page");
+    let homePageEl = document.getElementById("home-page");
+    let loginButtonEl = document.getElementById("login-button");
+
+    // If login button is clicked & account login is valid, hide the login page and proceed to the main menu
+    let loginButtonClicked = function() {
+        loginButtonEl.addEventListener("click", function() {
+           loginPageEl.hidden = true;
+           homePageEl.hidden = false;
+        });
+    };
 
 
-
+    loginButtonClicked();
 })();
