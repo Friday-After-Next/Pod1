22c94e63bf23d5a879e7dfde915fd26e28c1ef6e
Hung Ly <hunghly@gmail.com>
2019-12-15 15:57:26 -0600

    worked on local storage

---
+++ js/local_storage.js
@@ -0,0 +1,16 @@
+(function() {
+    "use strict";
+
+    // Create test username and password;
+    window.localStorage.setItem("username", "admin");
+    window.localStorage.setItem("password", "password");
+
+    const adminAccount = {
+        username: "admin",
+        password: "password"
+    }
+    window.localStorage.setItem("admin", JSON.stringify(adminAccount));
+
+
+
+})();
