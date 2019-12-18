414880910175d1440f76c5e9e706288a336006cf
Carlos Delgado III <carlos.delgado.dev@gmail.com>
2019-12-18 10:55:52 -0600

    minor changes to chores.

---
+++ js/chores.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// var chores = {
+//     laundry: document.getElementById('laundryStat'),
+//     dishes: document.getElementById('dishes-status'),
+//     grass: document.getElementById('grass-status')
+// };
+
+// var laundry = document.getElementById('laundryStat');
+//
+// var choresInt = setTimeout(needToDoChores, 5000);
+//
+//
+// function needToDoChores () {
+//     laundry.style.color = 'red';
+//     };
+//
+// laundry.addEventListener("click", needToDoChores);
+//
+// console.log(needToDoChores);
+
+
+//----- ALLOWS FOR POPOVER DIALOGUE WHEN PIG IS CLICKED. -----//
+$(function () {
+    $('[data-toggle="popover"]').popover()
+});
+
