db4ae55ac8e380d75fb20b39992a42ae6c92d19b
Hung Ly <hunghly@gmail.com>
2019-12-18 21:13:30 -0600

    worked on more of the link modals

--- js/app.js
+++ js/app.js
@@ -7,22 +7,24 @@
     let createAccountEl = document.getElementById("create-account-link");
     let accountCreationModalEl = document.getElementById("account-creation-container");
     let accountCreationButtonEl = document.getElementById("account-creation-button");
+    let accountCreationCancelButtonEl = document.getElementById("account-creation-cancel-button");
     let forgotAccountLinkEl = document.getElementById("forgot-account-link");
     let forgotAccountModalEl = document.getElementById("forgot-account-container");
-
-
-    let forgotPasswordLinkEl = document.getElementById("forgot-password-link");
-    let accountCreationCancelButtonEl = document.getElementById("account-creation-cancel-button");
     let forgotAccountCancelButtonEl = document.getElementById("forgot-account-cancel-button");
+    let forgotAccountFindButtonEl = document.getElementById("forgot-account-find-button");
+    let forgotPasswordLinkEl = document.getElementById("forgot-password-link");
+    let forgotPasswordModelEl = document.getElementById("forgot-password-container");
+
+
 // Create admin username and password;
-    let admin = {
+    const admin = {
         username: "admin",
         password: "password",
         balance: 0
     };
     window.localStorage.setItem("adminAccount", JSON.stringify(admin)); // place adminAccount in local storage
 // Create blank user account
-    let user = {
+    const user = {
         name: "",
         username: "",
         password: "",
@@ -31,7 +33,7 @@
     };
 
 // If login button is clicked & account login is valid, hide the login page and proceed to the home page
-    let loginButtonClicked = function () {
+    function loginButtonClicked() {
         loginButtonEl.addEventListener("click", function () {
             let usernameInput = document.getElementById("username-id").value;
             let passwordInput = document.getElementById("password-id").value;
@@ -56,19 +58,21 @@
     };
 
 // If create account link is clicked, display a modal that opens and allows user to create an account.
-    let createAccountLinkClicked = function () {
+    function createAccountLinkClicked() {
         createAccountEl.addEventListener("click", function () {
             createAccountModalTrigger();
         });
     };
 
-    let createAccountButtonClicked = function () {
+    // Will open account creation modal
+    function createAccountButtonClicked() {
         accountCreationButtonEl.addEventListener("click", function () {
             let nameInput = document.getElementById("account-creation-name").value;
             let emailInput = document.getElementById("account-creation-email").value;
             let usernameInput = document.getElementById("account-creation-username").value;
             let passwordInput = document.getElementById("account-creation-password").value;
 
+            // Checks usernameInput for validity and creates account in local storage
             if (usernameInput === "admin") {
                 alert("Cannot use admin as username.");
             } else if (nameInput && emailInput && usernameInput && passwordInput) {
@@ -85,32 +89,66 @@
         });
     };
 
-    let createAccountCancelButtonClicked = function () {
+    // Will close the modal if cancel is clicked
+    function createAccountCancelButtonClicked() {
         accountCreationCancelButtonEl.addEventListener("click", function () {
             createAccountModalTrigger();
         });
     };
 
-    let forgotAccountCancelButtonClicked = function() {
-        forgotAccountCancelButtonEl.addEventListener("click", function() {
-            forgotAccountModalTrigger();
-        });
-    }
-
-    let forgotAccountLinkClicked = function() {
-       forgotAccountLinkEl.addEventListener("click", function() {
-           forgotAccountModalTrigger();
-       });
-    };
-
+    // Toggles the modal open and close views
     function createAccountModalTrigger() {
         accountCreationModalEl.classList.toggle("close-modal-account-creation");
         accountCreationModalEl.classList.toggle("open-modal-account-creation");
     }
 
+    // Will open the modal when clicked
+    let forgotAccountLinkClicked = function () {
+        forgotAccountLinkEl.addEventListener("click", function () {
+            forgotAccountModalTrigger();
+        });
+        // window.addEventListener("click",function(e) {
+        //     console.log(e);
+        // });
+    };
+
+    let forgotAccountFindButtonClicked = function() {
+        forgotAccountFindButtonEl.addEventListener("click", function (){
+            let emailInput = document.getElementById("forgot-account-username").value;
+            console.log(emailInput);
+            if (emailInput === "") {
+                alert("Please enter a valid email.");
+            } else if (emailInput === user.email) {
+                alert("Your username: " + user.username);
+            } else {
+                alert("Incorrect email format or no user associated with email.")
+            }
+        });
+    };
+
+    // Will close the modal if cancel is clicked
+    function forgotAccountCancelButtonClicked() {
+        forgotAccountCancelButtonEl.addEventListener("click", function () {
+            forgotAccountModalTrigger();
+        });
+    }
+
+    // Toggles the modal open and close views
     function forgotAccountModalTrigger() {
         forgotAccountModalEl.classList.toggle("close-modal-forgot-account");
         forgotAccountModalEl.classList.toggle("open-modal-forgot-account");
+    }
+
+    let forgotPasswordLinkClicked = function () {
+        forgotPasswordLinkEl.addEventListener("click", function() {
+            forgotPasswordModalTrigger();
+        })
+    };
+
+    // Toggles the modal open and close views
+    function forgotPasswordModalTrigger() {
+        forgotPasswordModelEl.classList.toggle("close-modal-forgot-password");
+        forgotPasswordModelEl.classList.toggle("open-modal-forgot-password");
     }
 
     loginButtonClicked();
@@ -118,5 +156,7 @@
     createAccountButtonClicked();
     createAccountCancelButtonClicked();
     forgotAccountLinkClicked();
-    forgotAccountCancelButtonClicked()
+    forgotAccountFindButtonClicked();
+    forgotAccountCancelButtonClicked();
+    forgotPasswordLinkClicked();
 })();
