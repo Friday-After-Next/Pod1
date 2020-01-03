0d7e580cab0a9eb61e6cc00b013b920e90b250a2
Dalvin Cash <cashdalvin1@gmail.com>
2020-01-03 15:37:03 -0600

    updates to guess-the word

--- js/guess-the-word.js
+++ js/guess-the-word.js
@@ -5,61 +5,60 @@ let mistakes = 0;
 let guessed = [];
 let wordStatus = null;
 
+//generate random word
 function randomWord() {
-[;'']    answer =  wordBank[Math.floor(Math.random() * wordBank.length)];
+    answer =  wordBank[Math.floor(Math.random() * wordBank.length)];
 }
 
 //generate letter buttons
 function generateButtons() {
-    let buttonsHTML = "abcdefghijklmnopqrstuvwxyz".split("").map(letter => `<button class="btn btn-lrg btn-primary m-2" 
-id='` + letter + `' 
+    let buttonsHTML = "abcdefghijklmnopqrstuvwxyz".split("").map(letter => `<button class="btn btn-lrg btn-primary m-2"
+id='` + letter + `'
 onClick = "handleGuess('` + letter + `')">
 ` + letter + `
 </button>
 `).join("");
-
 document.getElementById("keyboard").innerHTML = buttonsHTML;
-
-    
 }
 function handleGuess(chosenLetter) {
     guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
     document.getElementById(chosenLetter).setAttribute('disabled', true);
-
-    if(answer.indexOf(chosenLetter) >= 0){
+    if(answer.indexOf(chosenLetter) >= 0) {
         guessedWord();
         checkIfGameWon();
     }else if(answer.indexOf(chosenLetter) === -1){
         mistakes++;
         updateMistakes();
+        checkIfGameLost();
+
     }
 }
-//Check if Game Won
-function checkIfGameWon() {
+//Check if game won
+function checkIfGameWon(){
     if(wordStatus === answer){
-        document.getElementById("keyboard").innerHTML = "You Won!";
-    }
-}
-//Check if Game Lost
-function checkIfGameLost() {
-    if(mistakes === maxWrong){
-        document.getElementById("wordSpotLight").innerHTML = "The answer was: " + answer;
-        document.getElementById("keyboard").innerHTML = "You Lost!";
+        document.getElementById('keyboard').innerHTML = 'You Won!!!';
     }
 }
 
-//word spotlight area
+//check if game lost
+function checkIfGameLost(){
+    if(mistakes === maxWrong) {
+        document.getElementById('wordSpotLight').innerHTML =  'The answer was: ' + answer;
+        document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+    }
+}
+
+
 function guessedWord(){
-   wordStatus = answer.split("").map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
-   document.getElementById("wordSpotLight").innerHTML = wordStatus;
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+    document.getElementById("wordSpotLight").innerHTML = wordStatus;
 }
 
-//mistakes
-function updateMistakes() {
-    document.getElementById("mistakes").innerHTML = mistakes;
+function updateMistakes(){
+    document.getElementById('mistakes').innerHTML = mistakes;
 }
 
-//Reset Button
+//reset button
 function reset(){
     mistakes = 0;
     guessed = [];
@@ -68,10 +67,7 @@ function reset(){
     updateMistakes();
     generateButtons();
 }
-
-//wrong guesses
 document.getElementById("maxWrong").innerHTML = maxWrong;
-
 
 randomWord();
 generateButtons();
